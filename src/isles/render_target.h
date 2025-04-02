bd0800e9b04948b1ba88229846b3cbc51bb38251
Carlos Rivera <cr4185320@gmail.com>
2025-04-02 00:43:48 -0500

    Update render_target.h

--- src/isles/render_target.h
+++ src/isles/render_target.h
@@ -2,13 +2,13 @@
 #define RENDER_TARGET_H
 
 struct Render_Target {
-  int width;
-  int height;
-  void *texture;
-  void *render_target_view;
-  void *depth_stencil_view;
+  int width = 0;
+  int height = 0;
+  void *texture = nullptr;
+  void *render_target_view = nullptr;
+  void *depth_stencil_view = nullptr;
 };
 
-internal Render_Target *make_render_target(int width, int height, DXGI_FORMAT format);
+internal Render_Target *make_render_target(int width, int height, DXGI_FORMAT format, DXGI_FORMAT depth_format = DXGI_FORMAT_D24_UNORM_S8_UINT);
 
 #endif // RENDER_TARGET_H
