96e7261f16e059a7b7833e708e4896a8452ee702
Carlos Rivera <cr4185320@gmail.com>
2025-04-21 23:54:31 -0500

    load animation data for skinned meshes

--- src/isles/mesh.cpp
+++ src/isles/mesh.cpp
@@ -15,10 +15,17 @@ internal inline Vector4 to_vec4(aiColor4D v) {
   return result;
 }
 
+internal inline Matrix4 to_matrix4(aiMatrix4x4 m) {
+  Matrix4 M;
+  M.columns[0] = Vector4(m.a1, m.b1, m.c1, m.d1);
+  M.columns[1] = Vector4(m.a2, m.b2, m.c2, m.d2);
+  M.columns[2] = Vector4(m.a3, m.b3, m.c3, m.d3);
+  M.columns[3] = Vector4(m.a4, m.b4, m.c4, m.d4);
+  return M;
+}
+
 internal Triangle_Mesh *load_mesh(std::string file_name) {
-  u32 import_flags = aiProcess_Triangulate;
-  import_flags |= aiProcess_FlipUVs;
-  import_flags |= aiProcess_GenNormals;
+  u32 import_flags = aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals | aiProcess_PopulateArmatureData;
 
   const aiScene *scene = assimp_importer.ReadFile(file_name.c_str(), import_flags);
   if (!scene) {
@@ -26,6 +33,8 @@ internal Triangle_Mesh *load_mesh(std::string file_name) {
     logprint("Error: %s\n", assimp_importer.GetErrorString());
     return nullptr;
   }
+
+  bool has_animation = scene->mNumAnimations > 0;
 
   Triangle_Mesh *mesh = new Triangle_Mesh();
 
@@ -111,7 +120,160 @@ internal Triangle_Mesh *load_mesh(std::string file_name) {
   }
   mesh->vertex_buffer = make_vertex_buffer(vertices.data, vertices.count, sizeof(Vertex_XNCUU));
 
+  //@Note Animation
+
+  read_animation(scene, mesh);
+
   return mesh;
+}
+
+internal Bone *bone_create(std::string name, int id, aiNodeAnim *channel) {
+  Bone *bone = new Bone;
+  bone->name = name;
+  bone->id = id;
+  bone->local_transform = make_matrix4(1.0f);
+
+  bone->translations.reserve(channel->mNumPositionKeys);
+  for (uint i = 0; i < channel->mNumPositionKeys; i++) {
+    aiVectorKey v = channel->mPositionKeys[i];
+    Key_Vector key;
+    key.time = v.mTime;
+    key.value = to_vec3(v.mValue);
+    bone->translations.push(key);
+  }
+
+  bone->rotations.reserve(channel->mNumRotationKeys);
+  for (uint i = 0; i < channel->mNumRotationKeys; i++) {
+    aiQuatKey v = channel->mRotationKeys[i];
+    Key_Quat key;
+    key.time = v.mTime;
+    key.value = Quaternion(v.mValue.x, v.mValue.y, v.mValue.z, v.mValue.w);
+    bone->rotations.push(key);
+  }
+
+  bone->scalings.reserve(channel->mNumScalingKeys);
+  for (uint i = 0; i < channel->mNumScalingKeys; i++) {
+    aiVectorKey v = channel->mScalingKeys[i];
+    Key_Vector key;
+    key.time = v.mTime;
+    key.value = to_vec3(v.mValue);
+    bone->scalings.push(key);
+  }
+
+  return bone;
+}
+
+internal void read_animation(const aiScene *scene, Triangle_Mesh *mesh) {
+  if (scene->mNumAnimations == 0) return;
+
+  mesh->animated = true;
+
+  uint vertex_idx = 0;
+  for (uint i = 0; i < scene->mNumMeshes; i++) {
+    aiMesh *amesh = scene->mMeshes[i];
+    Vertex_Skinned v;
+    for (uint j = 0; j < amesh->mNumVertices; j++) {
+      Vector3 p = to_vec3(amesh->mVertices[j]);
+      Vector3 n = to_vec3(amesh->mNormals[j]);
+      Vector2 uv = Vector2();
+      if (amesh->HasTextureCoords(0)) {
+        uv = to_vec2(amesh->mTextureCoords[0][j]);
+      }
+      v.position = p;
+      v.normal = n;
+      v.uv = uv;
+      v.bone_weights[0] = v.bone_weights[1] = v.bone_weights[2] = v.bone_weights[3] = 0;
+      v.bone_ids[0] = v.bone_ids[1] = v.bone_ids[2] = v.bone_ids[3] = -1;
+      mesh->skinned_vertices.push(v); 
+    }
+
+    for (u32 j = 0; j < amesh->mNumBones; j++) {
+      aiBone *abone = amesh->mBones[j];
+      std::string bone_name = abone->mName.C_Str();
+      int bone_id = -1;
+      if (mesh->bone_info_map.find(bone_name) != mesh->bone_info_map.end()) {
+        bone_id = mesh->bone_info_map[bone_name].id;
+      } else {
+        bone_id = mesh->bone_counter++;
+        Bone_Info bone_info;
+        bone_info.id = bone_id;
+        bone_info.offset_matrix = to_matrix4(abone->mOffsetMatrix);
+        mesh->bone_info_map.insert({bone_name, bone_info});
+      }
+
+      for (u32 w = 0; w < abone->mNumWeights; w++) {
+        aiVertexWeight weight = abone->mWeights[w];
+        Vertex_Skinned *vertex = &mesh->skinned_vertices.data[vertex_idx + weight.mVertexId];
+        set_vertex_bone_data(vertex, bone_id, weight.mWeight);
+      }
+    }
+
+    for (uint i = 0; i < amesh->mNumFaces; i++) {
+      aiFace *face = amesh->mFaces + i;
+      assert(face->mNumIndices == 3);
+      mesh->skinned_indices.push(face->mIndices[0]);
+      mesh->skinned_indices.push(face->mIndices[1]);
+      mesh->skinned_indices.push(face->mIndices[2]);
+    }
+
+    vertex_idx += amesh->mNumVertices;
+  }
+
+  mesh->anim_root = new Animation_Node;
+
+  for (uint i = 0; i < scene->mNumAnimations; i++) {
+    aiAnimation *ai_anim = scene->mAnimations[i];
+
+    std::string name = ai_anim->mName.C_Str();
+
+    Animation *animation = new Animation();
+    animation->mesh = mesh;
+    animation->name = name;
+    animation->duration = ai_anim->mDuration;
+    animation->ticks_per_second = ai_anim->mTicksPerSecond;
+
+    for (uint channel_idx = 0; channel_idx < ai_anim->mNumChannels; channel_idx++) {
+      aiNodeAnim *channel = ai_anim->mChannels[channel_idx];
+      std::string node_name = channel->mNodeName.data;
+
+      if (mesh->bone_info_map.find(node_name) == mesh->bone_info_map.end()) {
+        Bone_Info info = {};
+        info.id = mesh->bone_counter++;
+        mesh->bone_info_map.insert({node_name, info});
+      }
+
+      Bone *bone = bone_create(node_name, mesh->bone_info_map[node_name].id, channel);
+      animation->bones.push(bone);
+    }
+
+    mesh->animation_map.insert({name, animation});
+  }
+
+  read_animation_node(scene->mRootNode, mesh->anim_root);
+
+
+  mesh->skinned_vertex_buffer = make_vertex_buffer(mesh->skinned_vertices.data, mesh->skinned_vertices.count, sizeof(Vertex_Skinned));
+  mesh->skinned_index_buffer = make_index_buffer(mesh->skinned_indices.data, mesh->skinned_indices.count * sizeof(u32));
+}
+
+internal void read_animation_node(aiNode *node, Animation_Node *anim_node) {
+  anim_node->name = node->mName.data;
+  anim_node->transform = to_matrix4(node->mTransformation);
+  for (uint i = 0; i < node->mNumChildren; i++) {
+    Animation_Node *child = new Animation_Node;
+    read_animation_node(node->mChildren[i], child);
+    anim_node->children.push(child);
+  }
+}
+
+internal void set_vertex_bone_data(Vertex_Skinned *vertex, int bone_id, f32 weight) {
+  for (int i = 0; i < MAX_BONE_WEIGHTS; i++) {
+    if (vertex->bone_ids[i] == -1) {
+      vertex->bone_ids[i] = bone_id;
+      vertex->bone_weights[i] = weight;
+      break;
+    }
+  }
 }
 
 internal Triangle_Mesh *generate_plane_mesh(Vector2 size) {
--- src/isles/mesh.h
+++ src/isles/mesh.h
@@ -1,10 +1,37 @@
 #if !defined(MESH_H)
 #define MESH_H
 
+#define MAX_BONES 100
+#define MAX_BONE_WEIGHTS 4
+
 struct Texture;
+struct Triangle_Mesh;
+
+struct Vertex_Skinned {
+  Vector3 position;
+  Vector3 normal;
+  Vector2 uv;
+  int bone_ids[MAX_BONE_WEIGHTS];
+  f32 bone_weights[MAX_BONE_WEIGHTS];
+};
 
 struct Material {
   Texture *texture;
+};
+
+struct Bone_Info {
+  int id;
+  Matrix4 offset_matrix;
+};
+
+struct Key_Vector {
+  f64 time;
+  Vector3 value;
+};
+
+struct Key_Quat {
+  f64 time;
+  Quaternion value;
 };
 
 struct Triangle_List_Info {
@@ -13,7 +40,37 @@ struct Triangle_List_Info {
   u32 first_index;
 };
 
+enum Mesh_Flags {
+  MESH_FLAG_ANIMATED = (1<<0),
+};
+
+struct Bone {
+  std::string name;
+  int id;
+  Matrix4 local_transform;
+
+  Auto_Array<Key_Vector> translations;
+  Auto_Array<Key_Quat> rotations;
+  Auto_Array<Key_Vector> scalings; 
+};
+
+struct Animation_Node {
+  std::string name;
+  Matrix4 transform;
+  Auto_Array<Animation_Node*> children;
+};
+
+struct Animation {
+  std::string name;
+  f64 duration;
+  f64 ticks_per_second;
+
+  Triangle_Mesh *mesh;
+  Auto_Array<Bone*> bones;
+};
+
 struct Triangle_Mesh {
+  Mesh_Flags flags;
   String8 name;
   String8 file_path;
 
@@ -26,8 +83,22 @@ struct Triangle_Mesh {
 
   // Vertex_XNCUU
   ID3D11Buffer *vertex_buffer = nullptr;
+
+  // Skinned data
+  b32 animated = 0;
+  int bone_counter = 0;
+  std::unordered_map<std::string, Bone_Info> bone_info_map;
+  std::unordered_map<std::string, Animation*> animation_map;
+  Animation_Node *anim_root = nullptr;
+  ID3D11Buffer *skinned_vertex_buffer = nullptr;
+  ID3D11Buffer *skinned_index_buffer = nullptr;
+  Auto_Array<u32> skinned_indices;
+  Auto_Array<Vertex_Skinned> skinned_vertices;
 };
 
+internal void set_vertex_bone_data(Vertex_Skinned *vertex, int bone_id, f32 weight);
+internal void read_animation(const aiScene *scene, Triangle_Mesh *mesh);
+internal void read_animation_node(aiNode *node, Animation_Node *anim_node);
 
 internal Triangle_Mesh *generate_plane_mesh(Vector2 size);
 
