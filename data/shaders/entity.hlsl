83545e616dab52aa142f2dd87a4c9cc5794ad750
Carlos Rivera <cr4185320@gmail.com>
2025-04-25 15:33:00 -0500

    redo shadowing

--- data/shaders/entity.hlsl
+++ data/shaders/entity.hlsl
@@ -1,3 +1,7 @@
+// Specular exponent for sharpness
+#define SPEC_EXP 16.0
+// Specular strength
+#define SPEC_AMOUNT 0.5
 
 cbuffer Constants : register(b0) {
   matrix xform;
@@ -7,6 +11,7 @@ cbuffer Constants : register(b0) {
   float3 light_direction;
   float use_override_color;
   float4 override_color;
+  float3 eye;
 };
 
 struct Vertex_Input {
@@ -22,14 +27,16 @@ struct Vertex_Output {
   float3 normal : NORMAL;
   float4 color : COLOR;
   float2 uv : TEXCOORD;
-  float4 shadow_pos : TEXCOORD1;
+  float4 shadow_coords : TEXCOORD1;
 };
 
 SamplerState diffuse_sampler : register(s0);
-SamplerState point_sampler   : register(s1);
+SamplerState shadow_sampler   : register(s1);
 
 Texture2D diffuse_texture : register(t0);
 Texture2D shadow_map      : register(t1);
+
+// #include "shading.hlsl"
 
 Vertex_Output vs_main(Vertex_Input input) {
   Vertex_Output output;
@@ -41,32 +48,106 @@ Vertex_Output vs_main(Vertex_Input input) {
     output.color = override_color;
   }
   output.uv = input.uv;
-  float4 shadow_h = mul(light_view_projection, float4(output.pos_w, 1.0));
-  output.shadow_pos = shadow_h * float4(0.5, -0.5, 1.0, 1.0) + (float4(0.5, 0.5, 0.0, 0.0) * shadow_h.w);
+  float4 light_view_position = mul(light_view_projection, float4(output.pos_w, 1.0));
+  output.shadow_coords = light_view_position * float4(0.5, -0.5, 1.0, 1.0) + (float4(0.5, 0.5, 0.0, 0.0) * light_view_position.w);
+  // output.shadow_coords = mul(light_view_projection, float4(output.pos_w, 1.0));
   return output;
 }
 
 float4 ps_main(Vertex_Output input) : SV_TARGET {
-  float4 diffuse_color = diffuse_texture.Sample(diffuse_sampler, input.uv);
+  float4 diffuse_color = float4(1, 1, 1, 1);
+  float4 texture_color = diffuse_texture.Sample(diffuse_sampler, input.uv);
 
-  float light_dot = max(dot(light_direction, normalize(input.normal)), 0.0);
   float4 ambient = 0.15 * light_color;
+  float2 suv = input.shadow_coords.xy;
+  float depth = input.shadow_coords.z;
+
+  float bias = 0.005;
+  float2 poisson_disk[16] = {
+    float2( -0.94201624, -0.39906216 ), 
+    float2( 0.94558609, -0.76890725 ), 
+    float2( -0.094184101, -0.92938870 ), 
+    float2( 0.34495938, 0.29387760 ), 
+    float2( -0.91588581, 0.45771432 ), 
+    float2( -0.81544232, -0.87912464 ), 
+    float2( -0.38277543, 0.27676845 ), 
+    float2( 0.97484398, 0.75648379 ), 
+    float2( 0.44323325, -0.97511554 ), 
+    float2( 0.53742981, -0.47373420 ), 
+    float2( -0.26496911, -0.41893023 ), 
+    float2( 0.79197514, 0.19090188 ), 
+    float2( -0.24188840, 0.99706507 ), 
+    float2( -0.81409955, 0.91437590 ), 
+    float2( 0.19984126, 0.78641367 ), 
+    float2( 0.14383161, -0.14100790 ) 
+  };
 
   float d = max(dot(-light_direction, input.normal), 0.0);
-  float4 diffuse = d * light_color;
 
-  input.shadow_pos.xyz = input.shadow_pos.xyz / input.shadow_pos.w;
-
-  float shadow_bias;
-  /* float shadow_bias = max(0.005 * (1.0 - dot(input.normal, -light_direction)), 0.005); */
-  shadow_bias = 0.001;
-
-  float shadow = 0.0f;
-  if (shadow_map.Sample(point_sampler, input.shadow_pos.xy).r < input.shadow_pos.z - shadow_bias) {
-    shadow = 1.0f;
+  float shadow = 0.8;
+  for (int i = 0; i < 4; i++) {
+    int p = i;
+    float shadow_depth = shadow_map.Sample(shadow_sampler, suv + poisson_disk[p] * 0.001).r;
+    if (shadow_depth < depth - bias) {
+      shadow -= 0.2;
+    }
   }
 
-  float4 lighting = (ambient + (1.0 - shadow) * diffuse) * diffuse_color * input.color;
-  float4 final_color = float4(lighting.xyz, 1.0);
-  return final_color;
+  float4 color = ambient + shadow * diffuse_color * d;
+  color = saturate(color);
+  color = color * texture_color * input.color;
+  // color = float4(shadow, shadow, shadow, 1.0);
+
+  return color;
 }
+
+
+// void main() {
+// 	//Discard below the alpha threshold
+// 	vec4 col = texture2D(gm_BaseTexture, v_coord);
+// 	if (col.a<0.5) discard;
+	
+// 	//Factor in vertex color
+// 	col *= v_color;
+// 	//Convert to linear RGB
+// 	col.rgb = pow(col.rgb, vec3(GAMMA));
+	
+// 	//Compute shadow-projection-space coordinates
+// 	vec4 proj = u_sha_proj * v_shadow;
+	
+// 	//Normalize to the -1 to +1 range (accounting for perspective)
+// 	vec2 suv = proj.xy/proj.w;
+// 	//Edge vignette from shadow uvs
+// 	vec2 edge = max(1.0 - suv*suv, 0.0);
+// 	//Shade anything outside of the shadow map
+// 	float shadow = edge.x * edge.y * float(proj.z>0.0);
+// 	//Normalize shadow-space normals
+// 	vec3 norm = normalize(v_normal);
+	
+// 	//Compute slope with safe limits
+// 	float slope = 1.0 / max(-norm.z, 0.1);
+// 	//Only do shadow mapping inside the shadow map
+// 	if (shadow>0.01) shadow *= shadow_soft(proj, slope);
+// 	//Try alternative shadow functions here: shadow_hard(proj), shadow_interp(proj, slope)
+	
+// 	//Soft lighting
+// 	float lig = max(-norm.z, 0.5-0.5*norm.z);
+// 	//Blend with shadows and some ambient light
+// 	lig *= lig * (shadow*0.95 + 0.05);
+	
+// 	//Specular reflection
+// 	vec3 eye = normalize(v_eye);
+// 	float ref = max(reflect(eye, norm).z, 0.0);
+// 	float spec = pow(ref, SPEC_EXP) * SPEC_AMOUNT;
+// 	//Screen blend specular highlights with 
+// 	col.rgb = 1.0 - (1.0 - col.rgb*lig) * (1.0 - spec);
+	
+// 	//Convert back to sRGB
+// 	col.rgb = pow(col.rgb, 1.0/vec3(GAMMA));
+// 	//col.rgb = sin(eye/.1)*.5+.5;
+	
+// 	//Colorless test
+// 	//col.rgb = vec3(shadow);
+//     gl_FragColor = col;
+// }
+
