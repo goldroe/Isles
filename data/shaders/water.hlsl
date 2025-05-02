11e5ea41d2fe35d05b24e54f60613bbcd9f20cd0
Carlos Rivera <cr4185320@gmail.com>
2025-05-02 16:53:30 -0500

    depth volume based murkiness

--- data/shaders/water.hlsl
+++ data/shaders/water.hlsl
@@ -1,11 +1,13 @@
 
 cbuffer Constants : register(b0) {
-  matrix xform;
+  matrix view_proj;
   matrix world;
   float3 eye_position;
   float tiling;
   float wave_strength;
   float move_factor;
+  float near = 0.01;
+  float far = 1000.0;
 };
 
 struct Vertex_Input {
@@ -14,8 +16,8 @@ struct Vertex_Input {
 };
 
 struct Vertex_Output {
-  float4 position : SV_POSITION;
   float4 position_h : POSITION;
+  float4 screen_space : SV_POSITION;
   float3 normal : NORMAL;
   float2 dudv : TEXCOORD;
   float3 to_eye : EYEDIR;
@@ -31,13 +33,18 @@ SamplerState main_sampler : register(s0);
 
 Vertex_Output vs_main(Vertex_Input input) {
   Vertex_Output output;
-  float3 position_w = mul(world, float4(input.position, 1.0)).xyz;
-  output.position_h = mul(xform, float4(input.position, 1.0));
-  output.position = output.position_h;
+  float4 position_w = mul(world, float4(input.position, 1.0));
+  output.position_h = mul(view_proj, position_w);
+  output.screen_space = output.position_h;
   output.normal = input.normal;
   output.dudv = float2(input.position.x * 0.5 + 0.5, input.position.z * 0.5 + 0.5) * tiling;
-  output.to_eye = eye_position - position_w;
+  output.to_eye = eye_position - position_w.xyz;
   return output;
+}
+
+float linearize_depth(float d, float near, float far) {
+  float z_n = 2.0 * d - 1.0;
+  return 2.0 * near * far / (far + near - z_n * (far - near));
 }
 
 float4 ps_main(Vertex_Output input) : SV_TARGET {
@@ -47,12 +54,10 @@ float4 ps_main(Vertex_Output input) : SV_TARGET {
   float2 reflect_uv = float2(uv.x, uv.y);
   float2 refract_uv = float2(uv.x, -uv.y);
 
-  float near = 0.1;
-  float far = 1000.0;
-
   float depth = depth_map.Sample(main_sampler, refract_uv).r;
-  float floor_distance = 2.0 * near * far / (far + near - (2.0 * depth - 1.0) * (far - near));
-  float water_distance = 2.0 * near * far / (far + near - (2.0 * input.position.z - 1.0) * (far - near));
+  float floor_distance = linearize_depth(depth, near, far);
+  float water_distance = linearize_depth(input.position_h.z, near, far);
+
   float water_depth = (floor_distance - water_distance);
 
   float2 distort_uv = dudv_map.Sample(main_sampler, float2(input.dudv.x + move_factor, input.dudv.y)).xy * 0.1;
@@ -75,14 +80,13 @@ float4 ps_main(Vertex_Output input) : SV_TARGET {
   refract_factor = pow(refract_factor, 6.0);
   refract_factor = clamp(refract_factor, 0.0, 0.9);
 
-  float4 murky = float4(0.0, 0.3, 0.4, 0.5);
+  float4 murky = float4(0.0, 0.1, 0.04, 0.4);
 
   float4 reflect_color = reflection_texture.Sample(main_sampler, reflect_uv);
   float4 refract_color = refraction_texture.Sample(main_sampler, refract_uv);
-  refract_color = lerp(refract_color, murky, clamp(water_depth/60.0, 0.0, 1.0));
+  refract_color = lerp(refract_color, murky, clamp(water_depth * 2.0f, 0.0, 1.0));
 
   float4 final_color = lerp(reflect_color, refract_color, refract_factor);
-  final_color.a = clamp(water_depth * 12.0, 0.0, 1.0);
-  final_color = lerp(final_color, float4(0.0, 0.3, 0.5, 1.0), 0.2);
+  final_color.a = clamp(water_depth * 10.0f, 0.0, 1.0);
   return final_color;
 }
